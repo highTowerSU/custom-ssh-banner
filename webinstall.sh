728031f9b93065ca2c7497488e86d1eab5d1978d
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-27 01:17:19 +0200

    Create webinstall.sh

---
+++ webinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Überprüfen, ob git installiert ist
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Attempting to install..."
+    if command -v apt &> /dev/null; then
+        sudo apt update
+        sudo apt install -y git
+    else
+        echo "Error: Package manager not supported or git could not be installed."
+        exit 1
+    fi
+fi
+
+# Temporäres Verzeichnis erstellen und Repository klonen
+TEMP_DIR=$(mktemp -d)
+git clone https://github.com/highTowerSU/custom-ssh-banner.git "$TEMP_DIR"
+
+# Installationsskript ausführen
+chmod +x "$TEMP_DIR/install.sh"
+cd $TEMP_DIR
+"install.sh" "$@"
+cd ..
+
+# Aufräumen
+rm -rf "$TEMP_DIR"
