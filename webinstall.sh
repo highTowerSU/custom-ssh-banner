5ae25e71049731e6f675c6de3909359504ed46d4
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-27 01:29:54 +0200

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Überprüfen, ob git installiert ist
-if ! command -v git &> /dev/null; then
+if ! command -v git > /dev/null; then
     echo "Git is not installed. Attempting to install..."
-    if command -v apt &> /dev/null; then
+    if command -v apt > /dev/null; then
         sudo apt update
         sudo apt install -y git
     else
@@ -16,11 +16,17 @@ fi
 TEMP_DIR=$(mktemp -d)
 git clone https://github.com/highTowerSU/custom-ssh-banner.git "$TEMP_DIR"
 
-# Installationsskript ausführen
-chmod +x "$TEMP_DIR/install.sh"
-cd $TEMP_DIR
-"./install.sh" "$@"
-cd ..
+# Sicherstellen, dass install.sh existiert und ausführbar ist
+if [ -f "$TEMP_DIR/install.sh" ]; then
+    chmod +x "$TEMP_DIR/install.sh"
+    cd $TEMP_DIR
+    "./install.sh" "$@"
+    cd ..
+else
+    echo "Error: install.sh not found in the repository."
+    exit 1
+fi
 
+ls -la $TEMP_DIR
 # Aufräumen
 rm -rf "$TEMP_DIR"
