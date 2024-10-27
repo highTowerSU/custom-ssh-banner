fbdaf2585616014db9619f617eb1a34f201cddcb
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-10-27 14:05:36 +0100

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# -----------------------------------------------------------------------------
+# webinstall.sh
+# Copyright (C) 2010-2024 highTowerSU
+#
+# Repository: https://github.com/highTowerSU/custom-ssh-banner
+#
+# Description: installs custom SSH banner script, supports optional cron setup 
+#              and non-interactive mode.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
 # Überprüfen, ob git installiert ist
 if ! command -v git > /dev/null; then
     echo "Git is not installed. Attempting to install..."
